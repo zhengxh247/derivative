33dab4a3cbb8dd7b50d4691dc3a705a837f92a6e
zhengxh <1550736428@qq.com>
2020-12-14 16:55:13 +0800

    接口修复

--- hypermarket-backend/hypermarket-browser/src/main/java/com/team/derivative/admin/controller/UserController.java
+++ hypermarket-backend/hypermarket-browser/src/main/java/com/team/derivative/admin/controller/UserController.java
@@ -2,10 +2,15 @@ package com.team.derivative.admin.controller;
 
 import com.team.derivative.admin.Vo.User;
 import com.team.derivative.admin.service.MyUserDetailsService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zhengxh
@@ -13,17 +18,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 @Slf4j
+@Api(tags = {"用户管理"}, description = "user")
 public class UserController {
 
     @Autowired
     private MyUserDetailsService service;
 
-    @PostMapping("/getAll")
-    public void login(@Validated @RequestBody User user) {
-        System.out.println(user.getUsername());
-        service.loadUserByUsername(user.getUsername());
-    }
-
+    @ApiOperation(value = "用户注册")
     @PostMapping("/register")
     public Boolean register(@Validated @RequestBody User user) {
         return service.register(user);
--- hypermarket-backend/hypermarket-provider/src/main/java/com/team/derivative/controller/BrowserSecurityController.java
+++ hypermarket-backend/hypermarket-provider/src/main/java/com/team/derivative/controller/BrowserSecurityController.java
@@ -3,31 +3,22 @@
  */
 package com.team.derivative.controller;
 
-import com.team.derivative.properties.properties.SecurityConstants;
 import com.team.derivative.properties.properties.SecurityProperties;
-import com.team.derivative.support.SimpleResponse;
 import com.team.derivative.support.SocialUserInfo;
-import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.social.connect.Connection;
 import org.springframework.social.connect.web.ProviderSignInUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 
 /**
@@ -49,31 +40,31 @@ public class BrowserSecurityController {
 	@Autowired
 	private ProviderSignInUtils providerSignInUtils;
 
-	/**
-	 * 当需要身份认证时，跳转到这里
-	 * 
-	 * @param request
-	 * @param response
-	 * @return
-	 * @throws IOException
-	 */
-	@RequestMapping(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
-	@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
-	public SimpleResponse requireAuthentication(HttpServletRequest request, HttpServletResponse response)
-			throws IOException {
-
-		SavedRequest savedRequest = requestCache.getRequest(request, response);
-
-		if (savedRequest != null) {
-			String targetUrl = savedRequest.getRedirectUrl();
-			logger.info("引发跳转的请求是:" + targetUrl);
-			if (StringUtils.endsWithIgnoreCase(targetUrl, ".html")) {
-				redirectStrategy.sendRedirect(request, response, securityProperties.getBrowser().getLoginPage());
-			}
-		}
-
-		return new SimpleResponse("访问的服务需要身份认证，请引导用户到登录页");
-	}
+//	/**
+//	 * 当需要身份认证时，跳转到这里
+//	 *
+//	 * @param request
+//	 * @param response
+//	 * @return
+//	 * @throws IOException
+//	 */
+//	@RequestMapping(SecurityConstants.DEFAULT_UNAUTHENTICATION_URL)
+//	@ResponseStatus(code = HttpStatus.UNAUTHORIZED)
+//	public SimpleResponse requireAuthentication(HttpServletRequest request, HttpServletResponse response)
+//			throws IOException {
+//
+//		SavedRequest savedRequest = requestCache.getRequest(request, response);
+//
+//		if (savedRequest != null) {
+//			String targetUrl = savedRequest.getRedirectUrl();
+//			logger.info("引发跳转的请求是:" + targetUrl);
+//			if (StringUtils.endsWithIgnoreCase(targetUrl, ".html")) {
+//				redirectStrategy.sendRedirect(request, response, securityProperties.getBrowser().getLoginPage());
+//			}
+//		}
+//
+//		return new SimpleResponse("访问的服务需要身份认证，请引导用户到登录页");
+//	}
 
 	@GetMapping(" ")
 	public SocialUserInfo getSocialUserInfo(HttpServletRequest request) {
