10c5a8ff87312f5fa8e539c9eff0651f14e8946c
shuaike <1217391324@qq.com>
2021-01-09 17:12:19 +0800

    购物车增加模拟数据

--- hypermarket-frontend/src/views/shoppingCart/index.vue
+++ hypermarket-frontend/src/views/shoppingCart/index.vue
@@ -31,7 +31,34 @@
               <div class="col-total">小计</div>
               <div class="col-action">操作</div>
             </div>
-            <div class="list-body"></div>
+            <div class="list-body">
+              <div class="list-item">
+                <div class="item-box">
+                  <div class="col col-check">
+                    <el-checkbox v-model="check"></el-checkbox>
+                  </div>
+                  <div class="col-img">
+                    <img
+                      src="//cdn.cnbj0.fds.api.mi-img.com/b2c-shopapi-pms/pms_1568104854.08717848.jpg"
+                      width="80"
+                      height="80"
+                    />
+                  </div>
+                  <div class="col col-name">
+                    <h3>米家金属签字笔 金色</h3>
+                  </div>
+                  <div class="col col-price">24.9元</div>
+                  <div class="col col-num">
+                    <el-input-number v-model="num" size="small">
+                    </el-input-number>
+                  </div>
+                  <div class="col col-total">24.9元</div>
+                  <div class="col col-action">
+                    <i class="el-icon-close"></i>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="cart-recommend"></div>
         </div>
@@ -45,7 +72,9 @@ export default {
   name: "ShoppingCart",
   data() {
     return {
-      isAllCheck: false
+      isAllCheck: false,
+      check: false,
+      num: 1
     };
   }
 };
@@ -112,30 +141,52 @@ export default {
         padding-right: 26px;
         color: #424242;
         display: flex;
-        .col-check {
-          width: 110px;
-          margin-left: 24px;
+      }
+      .list-body {
+        .item-box {
+          padding: 15px 26px 15px 0;
+          border-top: 1px solid #e0e0e0;
+          display: flex;
+          .col {
+            height: 84px;
+            line-height: 84px;
+          }
+          .col-total {
+            color: $colorPrimary;
+          }
         }
-        .col-img {
-          width: 120px;
+      }
+      .col-check {
+        width: 110px;
+        margin-left: 24px;
+      }
+      .col-img {
+        width: 120px;
+      }
+      .col-name {
+        width: 380px;
+        h3 {
+          font-size: 18px;
+          font-weight: 400;
         }
-        .col-name {
-          width: 380px;
-        }
-        .col-price {
-          width: 140px;
-          padding-right: 18px;
-        }
-        .col-num {
-          width: 150px;
-        }
-        .col-total {
-          width: 120px;
-          padding-right: 81px;
-        }
-        .col-action {
-          width: 80px;
-        }
+      }
+      .col-price {
+        width: 140px;
+        padding-right: 18px;
+        text-align: center;
+      }
+      .col-num {
+        width: 150px;
+        text-align: center;
+      }
+      .col-total {
+        width: 120px;
+        padding-right: 81px;
+        text-align: right;
+      }
+      .col-action {
+        width: 80px;
+        text-align: center;
       }
     }
   }
@@ -144,6 +195,16 @@ export default {
 
 <style lang="scss">
 .col-check {
+  .el-checkbox__inner {
+    width: 18px;
+    height: 18px;
+  }
+  .el-checkbox__inner::after {
+    height: 9px;
+    left: 5px;
+    top: 2px;
+    width: 4px;
+  }
   .el-checkbox__input.is-checked .el-checkbox__inner,
   .el-checkbox__input.is-indeterminate .el-checkbox__inner {
     background-color: $colorPrimary;
@@ -157,6 +218,31 @@ export default {
   }
   .el-checkbox__input.is-checked + .el-checkbox__label {
     color: #606266;
+  }
+}
+.col-num {
+  .el-input-number__decrease:hover:not(.is-disabled)
+    ~ .el-input
+    .el-input__inner:not(.is-disabled),
+  .el-input-number__increase:hover:not(.is-disabled)
+    ~ .el-input
+    .el-input__inner:not(.is-disabled) {
+    border-color: #dcdfe6;
+  }
+  .el-input-number__decrease,
+  .el-input-number__increase {
+    background: #fff;
+  }
+  .el-input-number__decrease:hover,
+  .el-input-number__increase:hover {
+    color: #606266;
+    background: #e0e0e0;
+  }
+  .el-input-number__decrease {
+    border-right: 0;
+  }
+  .el-input-number__increase {
+    border-left: 0;
   }
 }
 </style>
