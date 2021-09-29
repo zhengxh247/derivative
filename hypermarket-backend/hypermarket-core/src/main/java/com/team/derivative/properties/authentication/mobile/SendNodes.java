06500e66844d59334865e3f757960c0ad0700701
zhengxh <1550736428@qq.com>
2021-09-29 20:22:08 +0800

    修改短信模板

--- hypermarket-backend/hypermarket-core/src/main/java/com/team/derivative/properties/authentication/mobile/SendNodes.java
+++ hypermarket-backend/hypermarket-core/src/main/java/com/team/derivative/properties/authentication/mobile/SendNodes.java
@@ -17,7 +17,7 @@ public class SendNodes {
 
     public Boolean sendNodes(String mobile, String code) {
 
-        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "<accessKeyId>", "<accessSecret>");
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI5tNJH2myQiwSNoTE6gqn", "HIInYzZIY8GcrhoEFe37RZhVS6TOMc");
         IAcsClient client = new DefaultAcsClient(profile);
 
         CommonRequest request = new CommonRequest();
@@ -29,7 +29,7 @@ public class SendNodes {
 //      短信签名，如【阿里云】，须申请，使用申请成功的签名
         request.putQueryParameter("SignName", "幸福商城");
 //      短信模板，须申请，使用申请成功的短信模板的模版CODE
-        request.putQueryParameter("TemplateCode", "SMS_10086***");
+        request.putQueryParameter("TemplateCode", "SMS_206550736");
 //      申请的短信模板中的变量code的值，如验证码，字符串格式必须为"{code:123051}"，否则报错，123051由代码生成
         request.putQueryParameter("TemplateParam", "{code:" + code + "}");
         try {
